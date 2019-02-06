a6c2696db6b21ed7ccd53eabf561707abcc7ed4d
Valeri <vakrus@ttu.ee>
2019-02-06 18:45:21 +0200

    profileRoute

---
+++ validation/profile.js
@@ -0,0 +1,62 @@
+const Validator = require('validator');
+const isEmpty = require('./is-empty');
+
+module.exports = function validateProfileInput(data) {
+  let errors = {};
+
+  data.handle = !isEmpty(data.handle) ? data.handle : '';
+  data.status = !isEmpty(data.status) ? data.status : '';
+  data.skills = !isEmpty(data.skills) ? data.skills : '';
+
+  if (!Validator.isLength(data.handle, { min: 2, max: 20 })) {
+    errors.handle = 'Handle must be between 2 and 30 characters.';
+  }
+
+  if (Validator.isEmpty(data.handle)) {
+    errors.handle = 'Handle is required.';
+  }
+
+  if (Validator.isEmpty(data.status)) {
+    errors.status = 'Status is required.';
+  }
+
+  if (Validator.isEmpty(data.skills)) {
+    errors.skills = 'Skills are required.';
+  }
+
+  if (!isEmpty(data.website)) {
+    if (!Validator.isURL(data.website)) {
+      errors.website = 'Invalid URL.';
+    }
+  }
+  if (!isEmpty(data.twitter)) {
+    if (!Validator.isURL(data.twitter)) {
+      errors.twitter = 'Invalid URL.';
+    }
+  }
+  if (!isEmpty(data.youtube)) {
+    if (!Validator.isURL(data.youtube)) {
+      errors.youtube = 'Invalid URL.';
+    }
+  }
+  if (!isEmpty(data.facebook)) {
+    if (!Validator.isURL(data.facebook)) {
+      errors.facebook = 'Invalid URL.';
+    }
+  }
+  if (!isEmpty(data.linkedin)) {
+    if (!Validator.isURL(data.linkedin)) {
+      errors.linkedin = 'Invalid URL.';
+    }
+  }
+  if (!isEmpty(data.instagram)) {
+    if (!Validator.isURL(data.instagram)) {
+      errors.instagram = 'Invalid URL.';
+    }
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
