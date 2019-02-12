b3c076206d3cff3b7ac92ba2aadbe2fe054a08bd
Valeri <vakrus@ttu.ee>
2019-02-12 18:55:11 +0200

    like logics

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,95 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Post = require('../../models/Post');
+const validatePostInput = require('../../validation/post');
+const Profile = require('../../models/Profile');
 
-// @route   GET api/posts/test
-// @desc    Tests post route
-// @access  Public
-router.get('/test', (req, res) =>
-  res.json({
-    msg: 'Posts works!'
-  })
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    if (!isValid) {
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+);
+
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.find()
+      .sort({ date: -1 })
+      .then(posts => res.json(posts))
+      .catch(err => res.status(404).json({ nopost: 'No posts found!' }));
+  }
+);
+
+router.get(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => res.json(post))
+      .catch(err => res.status(404).json({ nopost: 'Post not found!' }));
+  }
+);
+
+router.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (post.user.toString() !== req.user.id) {
+            return res.json(401).json({ notauthorized: 'User not authorized' });
+          }
+          post.remove().then(() => res.json({ success: true }));
+        })
+        .catch(err =>
+          res.status(404).json({ postnotfound: 'Post not found!' })
+        );
+    });
+  }
+);
+
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id)
+        .then(post => {
+          if (
+            post.likes.filter(like => like.user.toString() === req.user.id)
+              .length > 0
+          ) {
+            //Dislike
+            post.likes.shift({ user: req.user.id });
+          } else {
+            //Like
+            post.likes.unshift({ user: req.user.id });
+          }
+
+          post.save().then(post => res.json(post));
+        })
+        .catch(err => res.json(err));
+    });
+  }
 );
 
 module.exports = router;
